eec414eaa7fcbd8cd2c240daf003887ec6e704a6
Todd Wolfson <todd@twolfson.com>
2016-03-03 21:04:29 -0600

    Got iframe skipping working buuut now we have timeouts

--- test/integration-test/karma.conf.js
+++ test/integration-test/karma.conf.js
@@ -53,6 +53,10 @@ module.exports = function (config) {
     // how many browser should be started simultaneous
     concurrency: Infinity,
 
+    client: {
+        useIframe: false
+    },
+
     // Load in our module as a plugin
     // https://github.com/karma-runner/karma-chrome-launcher/blob/v0.2.2/examples/simple/karma.conf.js
     plugins: [
--- test/integration-test/test.js
+++ test/integration-test/test.js
@@ -4,7 +4,6 @@ var assert = require('assert');
 // Start our tests
 describe('A basic operation', function () {
   it('asserts without errors', function () {
-    console.log('wat');
-    assert.strictEqual(1 + 1, -1);
+    assert.strictEqual(1 + 1, 2);
   });
 });
