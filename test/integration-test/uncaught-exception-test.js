0722673476b456b19679b666c94600c043a821a0
Todd Wolfson <todd@twolfson.com>
2016-03-04 00:03:49 -0600

    Updated uncaught-exception test

--- test/integration-test/uncaught-exception-test.js
+++ test/integration-test/uncaught-exception-test.js
@@ -1,10 +1,2 @@
-// Load in our dependencies
-var assert = require('assert');
-
-// Start our tests
-// TODO: Add a test that fails (we can make it run based on environment variable in `karma.conf.js`)
-describe('A basic operation', function () {
-  it('asserts without errors', function () {
-    assert.strictEqual(1 + 1, 2);
-  });
-});
+// Verify Karma catches uncaught exceptions
+throw new Error('This error should not be caught and cause Karma to fail');
