aef86714fa76e375f9aea6f3fe0b9064f952cd20
Todd Wolfson <todd@twolfson.com>
2016-03-06 17:51:31 -0600

    Moved to preprocessor, working now

--- test/integration-test/karma.conf.js
+++ test/integration-test/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['electron', 'mocha'],
+    frameworks: ['mocha'],
 
     // list of files / patterns to load in the browser
     files: testFiles,
@@ -38,6 +38,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '*-test.js': ['electron']
     },
 
     // test results reporter to use
