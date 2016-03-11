7c015b95d8fdeb1ccc74797b3b72d9a5e161a94c
Todd Wolfson <todd@twolfson.com>
2016-03-11 13:47:50 -0600

    Updated comments

--- test/integration-test/failure-test.js
+++ test/integration-test/failure-test.js
@@ -1,4 +1,5 @@
 // Start our tests
+// DEV: This doesn't use `assert` to allow this to run with other runners
 describe('An invalid assertion', function () {
   it('raises an error', function () {
     // jscs:disable disallowYodaConditions
