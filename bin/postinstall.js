2204586518d60c5083a5bdbe00c3206ec3f520c6
Todd Wolfson <todd@twolfson.com>
2016-03-11 15:48:03 -0600

    Added postinstall.js

---
+++ bin/postinstall.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+// Load in our dependencies
+var path = require('path');
+var shelljs = require('shelljs');
+
+// Delete the existing `karma` install
+shelljs.rm('-r', path.join('node_modules', 'karma'));
+
+// Clone the karma repo
+shelljs.cd('node_modules');
+shelljs.exec('git clone https://github.com/twolfson/karma');
+shelljs.cd('karma');
+
+// Checkout our custom version
+shelljs.exec('git checkout 03273f2');
+
+// Install our dependencies and run the build
+shelljs.exec('npm install');
+shelljs.exec('npm run build');
