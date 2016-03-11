ad034c9b450d7dff70a3fa20573f50b93b4423ff
Todd Wolfson <todd@twolfson.com>
2016-03-11 13:46:45 -0600

    Updated failure-test as well

--- test/integration-test/failure-test.js
+++ test/integration-test/failure-test.js
@@ -1,9 +1,10 @@
-// Load in our dependencies
-var assert = require('assert');
-
 // Start our tests
 describe('An invalid assertion', function () {
   it('raises an error', function () {
-    assert.strictEqual(1, 2);
+    // jscs:disable disallowYodaConditions
+    if (1 !== 2) {
+      throw new Error('1 !== 2');
+    }
+    // jscs:enable disallowYodaConditions
   });
 });
