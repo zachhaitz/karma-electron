af2f19620aaa452abd24dbfb14c1e5a749e95247
Todd Wolfson <todd@twolfson.com>
2016-09-21 20:45:24 -0700

    Updated logs

--- test/reference/js/src-main.js
+++ test/reference/js/src-main.js
@@ -1,10 +1,10 @@
 // Log information about our module
-console.log('script-src: module.filename', module.filename);
+console.log('src-main: module.filename', module.filename);
   // /home/todd/github/karma-electron/test/reference/index.html
-console.log('script-src: module.exports', module.exports); // {}
-console.log('script-src: module.id', module.id); // .
-console.log('script-src: module.loaded', module.loaded); // true
-console.log('script-src: module.parent', module.parent); // null
+console.log('src-main: module.exports', module.exports); // {}
+console.log('src-main: module.id', module.id); // .
+console.log('src-main: module.loaded', module.loaded); // true
+console.log('src-main: module.parent', module.parent); // null
 
 // Load another script via `require`
 // DEV: Our path begins from perspective of `index.html` due to using `<script src=`
--- test/reference/js/src-submodule.js
+++ test/reference/js/src-submodule.js
@@ -1,9 +1,10 @@
 // Log information about our module
-console.log('browser2: module.filename', module.filename);
-  // /home/todd/github/karma-electron/test/reference/browser2.js
-console.log('browser2: module.exports', module.exports); // {}
-console.log('browser2: module.id', module.id); // /home/todd/github/karma-electron/test/reference/browser2.js
-console.log('browser2: module.loaded', module.loaded); // false
-console.log('browser2: module.parent', module.parent);
-  // {children: [...], exports: {}, filename: /home/todd/github/karma-electron/test/reference/index.html,
-  //    id: ., loaded: true, parent: null, paths: [...]}
+console.log('src-submodule: module.filename', module.filename);
+  // /home/todd/github/karma-electron/test/reference/js/src-submodule.js
+console.log('src-submodule: module.exports', module.exports); // {}
+console.log('src-submodule: module.id', module.id);
+  // /home/todd/github/karma-electron/test/reference/js/src-submodule.js
+console.log('src-submodule: module.loaded', module.loaded); // false
+console.log('src-submodule: module.parent', module.parent);
+  // {children: Array[8], exports: {}, filename: /home/todd/github/karma-electron/test/reference/index.html,
+  //    id: ., loaded: true, parent: null, paths: Array[6]}
