060de4b78f2a44282ff21fa770343efa820a83b0
Todd Wolfson <todd@twolfson.com>
2016-09-21 23:35:57 -0700

    Updated our test expectations

--- test/integration-test/node-require-test.js
+++ test/integration-test/node-require-test.js
@@ -9,9 +9,9 @@ var submodule = require('./test-files/submodule');
 // Start our tests
 describe('All `require` based Node.js integrations', function () {
   it('function as expected', function () {
-    // Example: /home/todd/github/karma-electron/test/integration-test/node-test.js
-    assert(/test[\/\\]integration-test[\/\\]node-test\.js$/.test(__filename),
-      'Expected "' + __filename + '" to end with "test/integration-test/node-test.js"');
+    // Example: /home/todd/github/karma-electron/test/integration-test/node-require-test.js
+    assert(/test[\/\\]integration-test[\/\\]node-require-test\.js$/.test(__filename),
+      'Expected "' + __filename + '" to end with "test/integration-test/node-require-test.js"');
     // Example: /home/todd/github/karma-electron/test/integration-test
     assert(/test[\/\\]integration-test$/.test(__dirname),
       'Expected "' + __dirname + '" to end with "test/integration-test"');
@@ -22,16 +22,17 @@ describe('module for `require` based Node.js integrations', function () {
   describe('in the top level', function () {
     // DEV: Determined exepctations via `../reference`
     it('identify as the page itself', function () {
-      // jscs:disable maximumLineLength
-      // DEV: `module.filename` should be something like
-      //   /home/todd/github/karma-electron/node_modules/electron-prebuilt/dist/resources/atom.asar/renderer/lib/init.js
-      //   since the page is HTTP, not a `file://`
-      // jscs:enable maximumLineLength
-      assert(module.filename);
+      // Example: /home/todd/github/karma-electron/test/integration-test/node-require-test.js
+      assert(/test[\/\\]integration-test[\/\\]node-require-test\.js$/.test(module.filename),
+        'Expected "' + module.filename + '" to end with "test/integration-test/node-require-test.js"');
       assert.strictEqual(typeof module.exports, 'object');
-      assert.strictEqual(module.id, '.');
+      // Example: /home/todd/github/karma-electron/test/integration-test/node-require-test.js
+      assert(/test[\/\\]integration-test[\/\\]node-require-test\.js$/.test(module.id),
+        'Expected "' + module.id + '" to end with "test/integration-test/node-require-test.js"');
       assert.strictEqual(submodule.loaded, true);
-      assert.strictEqual(module.parent, null);
+      // Example: /home/todd/github/karma-electron/node_modules/karma/static/context.html
+      assert(/karma[\/\\]static[\/\\]context\.html$/.test(module.parent.filename),
+        'Expected "' + module.parent.filename + '" to end with "karma/static/context.html"');
     });
   });
 
@@ -49,7 +50,7 @@ describe('module for `require` based Node.js integrations', function () {
 
       // Verify exported values
       assert.strictEqual(submodule.hello, 'world');
-      // Example: /home/todd/github/karma-electron/test/integration-test/node-test.js
+      // Example: /home/todd/github/karma-electron/test/integration-test/node-require-test.js
       assert(/test[\/\\]integration-test[\/\\]test-files[\/\\]submodule\.js$/.test(submodule.filename),
         'Expected "' + submodule.filename + '" to end with "test/integration-test/test-files/submodule.js"');
       // Example: /home/todd/github/karma-electron/test/integration-test
