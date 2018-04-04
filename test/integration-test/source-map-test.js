bd95278d409ebbb0aa10e871a2c0bd453e5beac6
Todd Wolfson <todd@twolfson.com>
2018-04-04 00:40:24 -0700

    Fixed up our source map test

--- test/integration-test/source-map-test.js
+++ test/integration-test/source-map-test.js
@@ -1,10 +1,7 @@
 // Load in our dependencies
 var assert = require('assert');
-var fs = require('fs');
 var request = require('request');
-// DEV: This resolve from `context.html` in `karma` so we need a lot of `../`
-// DEV: We avoid using `loadScriptsViaRequire` as we want to test trailing source map behavior
-var sourceMapTestFilepath = __dirname + '/../../../test/integration-test/source-map-test.js';
+var convertSourceMap = require('convert-source-map');
 
 // Start our tests
 describe('A file with a source map loaded with karma-electron', function () {
@@ -20,20 +17,20 @@ describe('A file with a source map loaded with karma-electron', function () {
     delete this.httpBody;
   });
 
-  it('preserves its source map', function () {
-    // Load in our fs lines
-    // DEV: We might have a trailing new line so we fallback
-    var fsLines = fs.readFileSync(sourceMapTestFilepath, 'utf8').split(/\n/g);
-    var fsLastLine = fsLines[fsLines.length - 1] || fsLines[fsLines.length - 2];
-
+  it('extends its source map', function () {
     // Load in our HTTP lines
-    var httpLines = this.httpBody.split(/\n/g);
-    var httpLastLine = httpLines[httpLines.length - 1];
+    var generatedContent = this.httpBody;
+    var generatedSourcemap = convertSourceMap.fromComment(generatedContent).toObject();
 
-    // Compare our fs to our HTTP last line
-    assert.strictEqual(fsLastLine, httpLastLine);
+    // Verify our original contents are in the source map
+    // {version: 3, sources: ['path/to/source-map-test.js', ...], names: [], mappings: 'AAAA;...', file: 'generated.js',
+    //   sourceRoot: '', sourcesContent: ['(function () {...', ...]}
+    assert.strictEqual(generatedSourcemap.sources.length, 2);
+    assert(generatedSourcemap.sources[1].endsWith('source-map-test.js'));
+    assert.strictEqual(generatedSourcemap.sourcesContent.length, 2);
+    assert.notEqual(generatedSourcemap.sourcesContent[1].indexOf('generatedContent'), -1);
   });
 });
 // Via https://github.com/thlorenz/convert-source-map
 // jscs:disable
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm9vLmpzIiwic291cmNlcyI6WyJjb25zb2xlLmxvZyhcImhpXCIpOyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsInNvdXJjZVJvb3QiOiIvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVpbGQvZm9vLm1pbi5qcyIsInNvdXJjZXMiOlsic3JjL2Zvby5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsInNvdXJjZVJvb3QiOiIvIn0=
